e9fcea73b744e18acd1528d31c7653b06c5ee290
Luismi21 <lrios390@accitesz.com>
2023-06-17 21:48:53 -0600

    ultima actualizacion

--- src/app/sobre-nosotros/sobre-nosotros.component.ts
+++ src/app/sobre-nosotros/sobre-nosotros.component.ts
@@ -3,13 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-sobre-nosotros',
   templateUrl: './sobre-nosotros.component.html',
-  styleUrls: ['./sobre-nosotros.component.css']
+  styleUrls: ['./sobre-nosotros.component.css'],
 })
 export class SobreNosotrosComponent implements OnInit {
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
